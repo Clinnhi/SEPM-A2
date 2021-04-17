42d17c3fe8e6f61fd7db19514caf3f36e60ce19c
s3742332 <s3742332@student.rmit.edu.au>
2021-04-17 12:19:40 +1000

    Fixed conflicts

--- addemployee.php
+++ addemployee.php
@@ -1,24 +1,58 @@
+<?php
+
+$host = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "SEPM";
+
+if (!empty($_POST['employeeEmail']) && !empty($_POST['employeePassword']) && !empty($_POST['employeePhoneNo']) && !empty($_POST['employeeFullName']) && !empty($_POST['employeeAddress'])) {
+  $email = $_POST['employeeEmail'];
+  $password = $_POST['employeePassword'];
+  $phone_number = $_POST['employeePhoneNo'];
+  $dob = $_POST['employeeDOB'];
+  $name = $_POST['employeeFullName'];
+  $address = $_POST['employeeAddress'];
+
+  $connection = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+  if ($connection->connect_errno) {
+    echo "Failed to connect to MySQL: " . $connection->connect_error;
+    exit();
+  }
+
+  $sql = $connection->prepare("INSERT INTO `Employee` (`email`, `password`, `phone_number`, `dob`, `name`, `address`) VALUES (?, ?, ?, ?, ?, ?)");
+  $sql->bind_param('ssssss',$email, $password, $phone_number, $dob, $name, $address);
+
+  if (!$sql->execute()) {
+    echo $sql->error;
+  }
+  $sql->close();
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>New Employee</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>New Employee</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link rel="stylesheet" href="addshifts.css">
+  <link rel="stylesheet" href="addshifts.css">
 
 </head>
+
 <body>
-    
+
   <!-- Nav Bar -->
   <ul class="nav nav-tabs">
     <li class="nav-item">
-      <a class="nav-link" aria-current="page" href="./mainmenu.html">Home</a>
+      <a class="nav-link" aria-current="page" href="./mainmenu.php">Home</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" aria-current="page" href="./addshifts.html">Shifts</a>
+      <a class="nav-link" aria-current="page" href="./addshifts.php">Shifts</a>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="/menu1.html">Menu Item</a>
@@ -37,46 +71,49 @@
     </div>
   </ul>
 
-  
+
   <div class="container">
-    <center> <h3 class="margin-top">Add New Employee</h3></center>
-     <div class="position-absolute top-50 start-50 translate-middle">
-        <form>
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Email address</label>
-              <input type="email" class="form-control" id="employeeEmail">
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">Password</label>
-              <input type="password" class="form-control" id="employeePassword">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Phone Number</label>
-                <input type="phone" class="form-control" id="employeePhoneNo" >
-              </div>
-              <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Date Of Birth</label>
-                <input type="email" class="form-control" id="employeeDOB" >
-              </div>
-              <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Full Name</label>
-                <input type="email" class="form-control" id="employeeFullName" >
-              </div>
-              <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Residential Address</label>
-                <input type="email" class="form-control" id="employeeAddress">
-              </div>
-          </form>
+    <center>
+      <h3 class="margin-top">Add New Employee</h3>
+    </center>
+    <div class="position-absolute top-50 start-50 translate-middle">
+      <form method="post" action="">
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Email address</label>
+          <input type="email" class="form-control" name="employeeEmail">
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Password</label>
+          <input type="password" class="form-control" name="employeePassword" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,8}" maxlength="8" title="Exactly 8 Charcters, 1 Uppercase, 1 Lower Case, 1 Number, 1 Special Character">
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Phone Number</label>
+          <input type="phone" class="form-control" name="employeePhoneNo">
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Date Of Birth</label>
+          <input type="date" class="form-control" name="employeeDOB" value="2021-06-01">
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Full Name</label>
+          <input type="text" class="form-control" name="employeeFullName">
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Residential Address</label>
+          <input type="text" class="form-control" name="employeeAddress">
+        </div>
     </div>
-</div>
+  </div>
 
 
   <div class="container">
     <div class="bottomright">
-        <button type="button" class="btn btn-primary">Add New Account</button>
+      <button type="submit" class="btn btn-primary">Add New Account</button>
     </div>
-</div>
+  </div>
 
+  </form>
 
 </body>
+
 </html>
--- addshifts.php
+++ addshifts.php
@@ -1,3 +1,36 @@
+<?php
+
+$host = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "SEPM";
+
+
+$connection = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+if ($connection->connect_errno) {
+    echo "Failed to connect to MySQL: " . $connection->connect_error;
+    exit();
+}
+$results = $connection->query("SELECT id, name FROM `Employee`");
+
+if (!empty($_POST['date']) && !empty($_POST['time']) && !empty($_POST['location']) && !empty($_POST['employee'])) {
+    $date = $_POST['date'];
+    $time = $_POST['time'];
+    $location = $_POST['location'];
+    $employee = $_POST['employee'][0];
+    
+
+
+$sql = $connection->prepare("INSERT INTO `Shifts` (`date`, `time`, `location`, `employee_id`) VALUES (?, ?, ?, ?)");
+$sql->bind_param('sssi',$date, $time, $location, $employee);
+if (!$sql->execute()) {
+    echo $sql->error;
+  }
+  $sql->close();
+
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,7 +49,7 @@
     <!-- Nav Bar -->
     <ul class="nav nav-tabs">
         <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="./mainmenu.html">Home</a>
+            <a class="nav-link" aria-current="page" href="./mainmenu.php">Home</a>
         </li>
         <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">Shifts</a>
@@ -31,7 +64,7 @@
             <a class="nav-link" href="#">Menu Item</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="./addemployee.html">Add New Employee</a>
+            <a class="nav-link" href="./addemployee.php">Add New Employee</a>
         </li>
         <div class="position-absolute top-0 end-0">
             <button class="btn btn-primary" type="button">Logout</button>
@@ -39,56 +72,59 @@
     </ul>
 
     <!-- Date/Time/Location -->
-    <div class="container">
-        <div class="margin-top">
-            <center> <h5>Please Enter The Shift Date, Time and Location</h5></center>
-            <div class="row g-3">
-                <div class="col-sm">
-                    <input type="text" class="form-control" placeholder="Shift Date" aria-label="State">
-                </div>
-                <div class="col-sm">
-                    <input type="text" class="form-control" placeholder="Shift Time" aria-label="Shift Time">
-                </div>
-                <div class="col-sm-7">
-                    <input type="text" class="form-control" placeholder="Shift Location" aria-label="Shift Location">
+    <form method="post" action="">
+        <div class="container">
+            <div class="margin-top">
+                <center>
+                    <h5>Please Enter The Shift Date, Time and Location</h5>
+                </center>
+                <div class="row g-3">
+                    <div class="col-sm">
+                        <input type="text" class="form-control" placeholder="Shift Date" aria-label="State" name="date" value="2021-06-01">
+                    </div>
+                    <div class="col-sm">
+                        <input type="text" class="form-control" placeholder="Shift Time" aria-label="Shift Time"
+                            name="time">
+                    </div>
+                    <div class="col-sm-7">
+                        <input type="text" class="form-control" placeholder="Shift Location" aria-label="Shift Location"
+                            name="location">
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="container">
-        <div class="position-absolute top-50 start-50 translate-middle">
-            <ul class="list-group">
-                <h5>Select Who Should Work The Shift</h5>
-                <li class="list-group-item">
-                    <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
-                    Steve Bob
-                </li>
-                <li class="list-group-item">
-                    <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
-                    Bob Steve
-                </li>
-                <li class="list-group-item">
-                    <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
-                    Harry Fu
-                </li>
-                <li class="list-group-item">
-                    <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
-                    Michael Jo
-                </li>
-                <li class="list-group-item">
-                    <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
-                    Joe Lama
-                </li>
-            </ul>
+        <div class="container">
+            <div class="position-absolute top-50 start-50 translate-middle">
+                <ul class="list-group">
+                    <h5>Select Who Should Work The Shift</h5>
+                    <?php
+                    if ($results->num_rows > 0) {
+                    foreach ($results as $employee) {
+                        ?>
+                    <li class="list-group-item">
+                        <input class="form-check-input me-1" type="radio" value="<?=$employee['id']?>" aria-label="..."
+                            name="employee[]">
+                        <?= $employee['name'] ?>
+                    </li>
+                    <?php
+                      }
+                    } else {
+                        ?>
+                    <h7>There are no employees available.</h7>
+                    <?php
+                    }
+                    ?>
+                </ul>
+            </div>
         </div>
-    </div>
+        <div class="container">
+            <div class="bottomright">
+                <button type="submit" class="btn btn-primary">Create New Shift</button>
+            </div>
+        </div>
+    </form>
 
-    <div class="container">
-        <div class="bottomright">
-            <button type="button" class="btn btn-primary">Create New Shift</button>
-        </div>
-    </div>
 </body>
 
 </html>

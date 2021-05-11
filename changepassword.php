9e509db0945ac26ad5606a960b950f73a0e547d9
Clinton Pham <Clintonjob@hotmial.co.uk>
2021-05-11 15:49:04 +1000

    Removed comments

--- changepassword.php
+++ changepassword.php
@@ -8,10 +8,10 @@ $dbPassword = "";
 $dbName = "SEPM";
 
 $connection = new mysqli($host, $dbUsername, $dbPassword, $dbName);
-// if ($connection->connect_errno) {
-//     echo "Failed to connect to MySQL: " . $connection->connect_error;
-//     exit();
-// }
+if ($connection->connect_errno) {
+    echo "Failed to connect to MySQL: " . $connection->connect_error;
+    exit();
+}
 
 
 

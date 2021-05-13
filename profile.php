23b8f86e0e69a4bc858d8eefe0fdf88e5a6cd164
Blackphoen1x <loveqiao361@gmail.com>
2021-05-14 00:47:26 +1000

    added profile page

---
+++ profile.php
@@ -0,0 +1,73 @@
+<?php
+session_start();
+
+$host = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "SEPM";
+
+$connection = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+if ($connection->connect_errno) {
+    echo "Failed to connect to MySQL: " . $connection->connect_error;
+    exit();
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update Details</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="addshifts.css">
+
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+
+    <div class="container margin-top">
+        <center>
+            <h1>My Profile</h1>
+        </center>
+        <hr>
+        <dl class="row">
+            <dt class="col-sm-4"></dt>
+            <dt class="col-sm-3">Full Name:</dt>
+            <dd class="col-sm-5">Ziyuan Wang</dd>
+
+            <dt class="col-sm-4"></dt>
+            <dt class="col-sm-3">Weekly Hour Limit:</dt>
+            <dd class="col-sm-5">40</dd>
+
+            <dt class="col-sm-4"></dt>
+            <dt class="col-sm-3">Preferred Name:</dt>
+            <dd class="col-sm-5">Atlas</dd>
+
+            <dt class="col-sm-4"></dt>
+            <dt class="col-sm-3">Phone Number:</dt>
+            <dd class="col-sm-5">0410000000</dd>
+
+            <dt class="col-sm-4"></dt>
+            <dt class="col-sm-3">Home Address:</dt>
+            <dd class="col-sm-5">1 King Street 3000</dd>
+
+            <dt class="col-sm-4"></dt>
+            <dt class="col-sm-3">Email:</dt>
+            <dd class="col-sm-5">atlas@gmail.com</dd>
+        </dl>
+        <hr>
+        <div class="d-flex justify-content-end">
+            <a href="updatedetails.php" class="btn btn-primary">Update my details</a>
+        </div>
+        </form>
+
+</body>
+
+</html>

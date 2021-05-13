2252ae042de741896c65c9922bf12ba256ae4c3f
Blackphoen1x <loveqiao361@gmail.com>
2021-05-14 02:34:54 +1000

    Update profile.php

--- profile.php
+++ profile.php
@@ -25,8 +25,6 @@ if ($connection->connect_errno) {
     <title>Update Details</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link rel="stylesheet" href="addshifts.css">
-
     <script type="text/javascript" src="https://dss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/js/lib/jquery-1-edb203c114.10.2.js"></script>
 
     <script>

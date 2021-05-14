516073f364516606d5f2bba042d675c36f47242f
Roger543200664 <70142494+Roger543200664@users.noreply.github.com>
2021-05-15 00:51:15 +1000

    Update schema.sql
    Add `preferred_name` in employee table

--- schema.sql
+++ schema.sql
@@ -9,6 +9,7 @@ CREATE TABLE `Employee` (
   `phone_number` varchar(14) NOT NULL,
   `dob` date NOT NULL,
   `name` varchar(70) NOT NULL,
+  `preferred_name` varchar(70) NOT NULL,
   `address` varchar(100) NOT NULL,
   `is_manager` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;

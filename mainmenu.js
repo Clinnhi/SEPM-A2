c9362f1da34d8626383ca8e2bb1f5e0bcb542bb4
Blackphoen1x <loveqiao361@gmail.com>
2021-05-17 02:29:43 +1000

    ajax part for visual representation

--- mainmenu.js
+++ mainmenu.js
@@ -1,91 +1,127 @@
 $(function () {
-    var weekdata = [12, 19, 3, 5, 2, 3, 6];
-    var monthdata = [2, 3, 5, 7, 3, 6, 8, 12, 6, 7, 2, 3, 5, 7, 3, 6, 8, 5, 3, 9, 2, 3, 5, 7, 3, 6, 8, 6, 11, 2];
-    var monthlabels = [];
 
-    monthlabels.length = monthdata.length;
-    for (var i = 0; i < monthlabels.length; i++) {
-        monthlabels[i] = i + 1;
+
+
+
+    $.ajax({
+        url: "getworkload.php",
+        type: "POST",
+        dataType: "json",
+        error: function () {
+            alert('Error loading XML document');
+        },
+        success: function (data, status) {
+            console.log(data);
+            createCharts(data);
+        }
+    });
+
+    var createCharts = function (data) {
+        var day = new Date();
+        var dayCount = new Date(day.getFullYear(), day.getMonth() + 1, 0).getDate();
+        //console.log(dayCount);
+        var weekdata = data.weekData;
+        var monthdata = data.monthData;
+        var monthlabels = [];
+        var i;
+
+        monthdata.splice(dayCount + 1, monthdata.length - dayCount);
+
+        monthlabels.length = monthdata.length;
+        for (i = 0; i < monthlabels.length; i++) {
+            monthlabels[i] = i + 1;
+        }
+
+        for (i = 0; i < weekdata.length; i++) {
+            weekdata[i] = parseInt(weekdata[i]);
+        }
+
+        for (i = 0; i < monthdata.length; i++) {
+            monthdata[i] = parseInt(monthdata[i]);
+        }
+
+        var ctx = document.getElementById("weekChart");
+        var weekChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ["Monday", "Tuesday", "Wednesday", "Tursday", "Friday", "Saturday", "Sunday"],
+                datasets: [{
+                    label: 'workload hours',
+                    data: weekdata,
+                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    borderWidth: 1,
+                }]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: "Current week workload" + "       Total hours:" + sum(weekdata),
+                    fontSize: 20,
+                    fontColor: '#000',
+                    padding: 30
+                },
+                legend: { display: false },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            callback: function (value, index, values) {
+                                return value + " h";
+                            }
+                        }
+                    }]
+                }
+            }
+        });
+
+        ctx = document.getElementById("monthChart");
+        var monthChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: monthlabels,
+                datasets: [{
+                    label: 'workload hours',
+                    data: monthdata,
+                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    borderWidth: 1,
+                }]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: "Current month workload" + "       Total hours:" + sum(monthdata),
+                    fontSize: 20,
+                    fontColor: '#000',
+                    padding: 30
+                },
+                legend: { display: false },
+                scales: {
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Day'
+                        },
+                        ticks: {
+                            beginAtZero: true,
+                        }
+                    }],
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            callback: function (value, index, values) {
+                                return value + " h";
+                            }
+                        }
+                    }]
+                }
+            }
+        });
+
+        $("#monthChart").hide();
+
     }
-
-    var ctx = document.getElementById("weekChart");
-    var weekChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ["Monday", "Tuesday", "Wednesday", "Tursday", "Friday", "Saturday", "Sunday"],
-            datasets: [{
-                label: 'workload hours',
-                data: weekdata,
-                backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                borderColor: 'rgba(54, 162, 235, 1)',
-                borderWidth: 1,
-            }]
-        },
-        options: {
-            title: {
-                display: true,
-                text: "Current week workload" + "       Total hours:" + sum(weekdata),
-                fontSize: 20,
-                fontColor: '#000',
-                padding: 30
-            },
-            legend: { display: false },
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true,
-                        callback: function (value, index, values) {
-                            return value + " h";
-                        }
-                    }
-                }]
-            }
-        }
-    });
-
-    ctx = document.getElementById("monthChart");
-    var monthChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: monthlabels,
-            datasets: [{
-                label: 'workload hours',
-                data: monthdata,
-                backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                borderColor: 'rgba(54, 162, 235, 1)',
-                borderWidth: 1,
-            }]
-        },
-        options: {
-            title: {
-                display: true,
-                text: "Current month workload" + "       Total hours:" + sum(monthdata),
-                fontSize: 20,
-                fontColor: '#000',
-                padding: 30
-            },
-            legend: { display: false },
-            scales: {
-                xAxes: [{
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Day'
-                    },
-                    ticks: {
-                        beginAtZero: true,
-                    }
-                }],
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true,
-                        callback: function (value, index, values) {
-                            return value + " h";
-                        }
-                    }
-                }]
-            }
-        }
-    });
 
     function sum(arr) {
         var s = 0;
@@ -106,5 +142,6 @@ $(function () {
         }
     });
 
-    $("#monthChart").hide();
+
+
 })

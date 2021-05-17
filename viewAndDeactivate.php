6ade5a7b2a9806b90471d9f9a02e72ac02e4baa1
s3742332 <s3742332@student.rmit.edu.au>
2021-05-17 18:21:32 +1000

    fixed deactivate issues

--- viewAndDeactivate.php
+++ viewAndDeactivate.php
@@ -16,9 +16,34 @@ $results = $connection->query("SELECT employee_id, name FROM `Employee`");
 
 if (isset($_POST['deactivate'])) {
     $deactivate = $_POST['deactivate'];
-    $sql = "DELETE FROM `Employee` WHERE id = $deactivate";
-    if ($connection->query($sql) == true) {
-        echo "Record deleted successfully";
+    
+    $sql1 = "DELETE FROM `hour_limits` WHERE employee_id = $deactivate";
+    $sql2 = "DELETE FROM `shifts` WHERE employee_id = $deactivate";
+    $sql3 = "DELETE FROM `unavailabilities` WHERE employee_id = $deactivate";
+    $sql4 = "DELETE FROM `Employee` WHERE employee_id = $deactivate";
+
+    // $sql = "DELETE `Employee`,`hour_limits`,`shifts`,`unavailabilities` FROM `Employee` INNER JOIN `hour_limits` INNER JOIN `shifts` INNER JOIN `unavailabilities` INNER JOIN WHERE employee.employee_id = $deactivate AND hour_limits.employee_id = $deactivate AND shifts.employee_id = $deactivate AND unavailabilities.employee_id = $deactivate";
+
+    if ($connection->query($sql1) == true) {
+        echo "Hour Limits deleted successfully ";
+    } else {
+        echo "Error deleting record: " . $connection->error;
+    }
+
+    if ($connection->query($sql2) == true) {
+        echo "Shifts deleted successfully ";
+    } else {
+        echo "Error deleting record: " . $connection->error;
+    }
+
+    if ($connection->query($sql3) == true) {
+        echo "Unavailabilities deleted successfully ";
+    } else {
+        echo "Error deleting record: " . $connection->error;
+    }
+
+    if ($connection->query($sql4) == true) {
+        echo "Employee deleted successfully ";
     } else {
         echo "Error deleting record: " . $connection->error;
     }
@@ -34,14 +59,18 @@ if (isset($_POST['deactivate'])) {
     <title>Employee List</title>
 
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+    </script>
     <link rel="stylesheet" href="addshifts.css">
 
-    <script type="text/javascript" src="https://dss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/js/lib/jquery-1-edb203c114.10.2.js"></script>
+    <script type="text/javascript"
+        src="https://dss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/js/lib/jquery-1-edb203c114.10.2.js"></script>
 
     <script>
-        $(function() {
+        $(function () {
             //remove active class for 'Home' and add active class for current page.
             $(".nav-item a[href|='./mainmenu.php']").removeClass("active");
             $(".nav-item a[href|='./viewAndDeactivate.php']").addClass("active");
@@ -62,17 +91,21 @@ if (isset($_POST['deactivate'])) {
                 if ($results->num_rows > 0) {
                     foreach ($results as $employee) {
                 ?>
-                        <li class="list-group-item"><?= $employee['name'] ?> <div class="btn-group position-absolute top-0 end-0" role="group" aria-label="Basic example">
-                                <button type="submit" class="btn btn-dark" name="view" value="<?= $employee['employee_id'] ?>">View Profile</button>
-                                <button type="submit" class="btn btn-secondary" name="edit" value="<?= $employee['employee_id'] ?>">Edit Profile</button>
-                                <button type="submit" class="btn btn-primary" name="deactivate" value="<?= $employee['employee_id'] ?>">Deactivate Account</button>
-                            </div>
-                        </li>
-                    <?php
+                <li class="list-group-item"><?= $employee['name'] ?> <div
+                        class="btn-group position-absolute top-0 end-0" role="group" aria-label="Basic example">
+                        <button type="submit" class="btn btn-dark" name="view"
+                            value="<?= $employee['employee_id'] ?>">View Profile</button>
+                        <button type="submit" class="btn btn-secondary" name="edit"
+                            value="<?= $employee['employee_id'] ?>">Edit Profile</button>
+                        <button type="submit" class="btn btn-primary" name="deactivate"
+                            value="<?= $employee['employee_id'] ?>">Deactivate Account</button>
+                    </div>
+                </li>
+                <?php
                     }
                 } else {
                     ?>
-                    <h2>There are no employees! Hire someone!!!</h2>
+                <h2>There are no employees! Hire someone!!!</h2>
                 <?php
                 }
                 ?>
